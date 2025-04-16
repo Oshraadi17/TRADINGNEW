d6abaf99f1514e7204b282de41355736ea19198e
Oshraadi17 <Oshraadibasis17@icloud.com>
2025-04-16 08:49:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ const canvas = document.getElementById('graphCanvas');
 const ctx = canvas.getContext('2d');
 
 function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight * 0.5;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight * 0.5;
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
 
 let offset = 0;
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#0080ff';
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.lineWidth = 1.8;
+  ctx.strokeStyle = '#ffffffaa';
 
-    for (let x = 0; x < canvas.width; x++) {
-        const y = canvas.height / 2 + Math.sin((x + offset) * 0.02) * 50;
-        ctx.lineTo(x, y);
-    }
+  for (let x = 0; x < canvas.width; x++) {
+    const y = canvas.height / 2 + Math.sin((x + offset) * 0.02) * 40;
+    ctx.lineTo(x, y);
+  }
 
-    ctx.stroke();
-    offset += 1;
-    requestAnimationFrame(draw);
+  ctx.stroke();
+  offset += 1;
+  requestAnimationFrame(draw);
 }
 draw();
