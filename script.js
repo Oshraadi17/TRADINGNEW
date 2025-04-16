a71452e523bdb751afe8d9165d9fb7cfdfaa9ab1
Oshraadi17 <Oshraadibasis17@icloud.com>
2025-04-16 05:00:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 
-const logo = document.getElementById("logo");
-const launchContent = document.getElementById("launch-content");
 const music = document.getElementById("bg-music");
 const muteBtn = document.getElementById("mute-btn");
 
-setTimeout(() => {
-  logo.style.display = "none";
-  launchContent.style.display = "block";
-}, 2500);
-
 muteBtn.addEventListener("click", () => {
-  music.muted = !music.muted;
-  muteBtn.textContent = music.muted ? "🔇" : "🔊";
+    music.muted = !music.muted;
+    muteBtn.textContent = music.muted ? "🔇" : "🔊";
 });
+
+function goHome() {
+    window.location.href = "home.html";
+}
